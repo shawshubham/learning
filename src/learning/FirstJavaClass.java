6d51555a0aed3df783016b269d8fe481c313fbf2
Shubham Shaw <shubhamshaw139@gmail.com>
2024-06-06 12:33:55 +0100

    initial java commit

---
+++ src/learning/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package learning;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing my java application");
+	}
+
+}
