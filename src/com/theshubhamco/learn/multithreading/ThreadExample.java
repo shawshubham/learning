47e061f45339e53b2769d450e871c7a6e65fb8ec
Shubham Shaw <shubhamshaw139@gmail.com>
2025-02-12 00:59:48 +0000

    Adding thread example
    Adding thread example

---
+++ src/com/theshubhamco/learn/multithreading/ThreadExample.java
@@ -0,0 +1,28 @@
+package com.theshubhamco.learn.multithreading;
+
+public class ThreadExample {
+
+	public static void main(String[] args) {
+		System.out.println("Main class having thread name: "+ Thread.currentThread().getName());
+		
+		MyThread thread = new MyThread();//creating thread
+		
+		/*
+		 * What if we we directly execute the run() method, it will just be treated as normal method
+		 * and executed by main thread
+		*/
+		thread.run();
+		
+		//Starting the thread
+		thread.start();
+	}
+
+}
+
+class MyThread extends Thread{
+	@Override
+	public void run() {
+		System.out.println("Thread running: " + Thread.currentThread().getName());
+	}
+	
+}
